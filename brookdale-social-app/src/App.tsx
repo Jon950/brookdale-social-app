b09f8bf5ae1d9c9bbb53e81a441d99bb49864705
BusinessOperatingSystem-IT <86786333+BusinessOperatingSystem-IT@users.noreply.github.com>
2021-09-15 09:01:02 -0400

    setup react

---
+++ brookdale-social-app/src/App.tsx
@@ -0,0 +1,17 @@
+// import React, {useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  // const [number, setNumber] = useState(0)
+
+  return (
+    <>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </>
+  );
+}
+
+export default App;
