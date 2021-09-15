b1a2a097b1d313a9c8b2dfb2dbeb8149cad7e3dd
BusinessOperatingSystem-IT <86786333+BusinessOperatingSystem-IT@users.noreply.github.com>
2021-09-15 11:35:20 -0400

    worked on the StarRatingBar

--- brookdale-social-app/src/App.tsx
+++ brookdale-social-app/src/App.tsx
@@ -1,16 +1,17 @@
 // import React, {useState} from 'react';
-import logo from './logo.svg';
+// import logo from './brookdale-community-college-mascot-logo.svg';
 import './App.css';
+
+// Pages
+import ProfileBox from "./components/subComponents/ProfileBox"
 
 function App() {
   // const [number, setNumber] = useState(0)
 
   return (
-    <>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </>
+    <div className="app">
+      <ProfileBox/>
+    </div>
   );
 }
 
